e93896b75a3c2180b396790d3c80ef35eb03a399
nzyuzin <nzyuzin93@gmail.com>
2013-04-08 16:48:11 +0400

    working on combat system

--- src/creatures/Creature.java
+++ src/creatures/Creature.java
@@ -3,15 +3,17 @@ package creatures;
 import movement.*;
 import map.Map;
 import map.exceptions.*;
-
-final class Attributes {
-	short strength;
-	short dexterity;
-	short intellegence;
-	short armor;
-}
+import java.util.Random;
 
 public abstract class Creature implements Movable {
+	
+	protected final class Attributes {
+		short strength;
+		short dexterity;
+		short intellegence;
+		short armor;
+	}
+	
 	static int id;
 	
 	public int posX = -1;
@@ -20,6 +22,7 @@ public abstract class Creature implements Movable {
 	
 	short currentHP;
 	short maxHP;
+	float attackRate;
 	String name;
 	Attributes attributes;
 	
@@ -33,24 +36,31 @@ public abstract class Creature implements Movable {
 			Map.moveCreature(this, there);
 				
 		} catch (CellIsTaken e) {
-			hit(there);
+			if (Map.canHit(this, there))
+				hit(there);
 		}
 	}
 	
-	int hit(Direction there) {
+	void hit(Direction there) {
 	/*  TODO
 	 *  Hit should generate integer which is calculated,
 	 *  depending on attackers attributes and random number.
 	 *  This integer is passed to takeAHit method later on,
 	 *  separately from this method.
 	 */
-		return 17;
+		Random rand = new Random();
+		int damage = rand.nextInt((int) (attributes.strength * attackRate));
+		Map.getCreature(this, there).takeAHit(damage);
 	}
+	
 	void takeAHit(int damage) {
 	/* TODO
-	 * if takes 0 as arguments attacker missed,
+	 * if takes 0 as arguments - attacker missed,
 	 * otherwise it should apply armor coefficient to damage and then subtract it from currenthp.	
 	 */
-		currentHP -= damage;
+		Random rand = new Random();
+		int chancetoevade = rand.nextInt(50) + attributes.dexterity;
+		if (chancetoevade < 50)
+			currentHP -= (damage - attributes.armor);
 	}
 }
